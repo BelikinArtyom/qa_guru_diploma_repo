029ca873ecc904d60ded28318f3960e963b8bfd3
Artyom Belikin <biloplus@yandex.ru>
2025-07-22 19:23:04 +0300

    Commit message zaglushka

--- src/test/java/tests/flatSearchAPITests/ApiLogin.java
+++ src/test/java/tests/flatSearchAPITests/ApiLogin.java
@@ -1,9 +1,19 @@
 package tests.flatSearchAPITests;
 
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
 public class ApiLogin {
 
 
+    @Test
+    void loginApiTest() {
+        given()
+                .post("https://sso-api.trendagent.ru/v1/login");
+        then().
 
+
+    }
 
 
 
