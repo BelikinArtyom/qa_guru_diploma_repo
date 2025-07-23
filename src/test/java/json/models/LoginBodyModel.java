966a29b87872ebed3214a7645586f59a6f04a514
Artyom Belikin <biloplus@yandex.ru>
2025-07-23 11:28:34 +0300

    Commit message zaglushka

---
+++ src/test/java/json/models/LoginBodyModel.java
@@ -0,0 +1,8 @@
+package json.models;
+
+public class LoginBodyModel {
+
+    String phone = "+79215711724";
+    String password = "1724";
+
+}
--- src/test/java/tests/flatSearchAPITests/ApiLogin.java
+++ src/test/java/tests/flatSearchAPITests/ApiLogin.java
@@ -2,6 +2,7 @@ package tests.flatSearchAPITests;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class ApiLogin {
 
@@ -9,8 +10,15 @@ public class ApiLogin {
     @Test
     void loginApiTest() {
         given()
-                .post("https://sso-api.trendagent.ru/v1/login");
-        then().
+                .log().uri()
+                .post("https://sso-api.trendagent.ru/v1/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(
+
+
+
 
 
     }
