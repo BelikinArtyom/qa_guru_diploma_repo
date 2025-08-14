210bb0c7ca2024c1899d46d2debd4c7a58f4ee2f
Artyom Belikin <biloplus@yandex.ru>
2025-08-14 23:29:08 +0300

    cookie update

--- src/test/java/helpers/CookieManager.java
+++ src/test/java/helpers/CookieManager.java
@@ -3,9 +3,12 @@ package helpers;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class CookieManager {
 
     public CookieManager setCityForTest() {
+        open("https://trendrealty.ru/images/favicons/favicon.ico");
         Cookie ck = new Cookie("selected_city", "58c665588b6aa52311afa01b");
         WebDriverRunner.getWebDriver().manage().addCookie(ck);
         return this;
